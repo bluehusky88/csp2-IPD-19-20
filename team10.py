1a82d0017fab635d31411054c0a52136420f7b2f
ansarimeilad05 <ansarimeilad@gmail.com>
2020-02-12 17:15:10 +0000

    The Yogurts team10.py

--- team10.py
+++ team10.py
@@ -1,13 +1,13 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
+      team_name = 'The Yogurts'
+     strategy_name: 'Success'
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'The Yogurts' # Only 10 chars displayed.
+strategy_name = 'Success'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
