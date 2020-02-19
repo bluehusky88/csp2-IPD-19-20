bdb28580a70185806027e9c11b58ca815d91b11d
kigupta5 <kigupta001@gmail.com>
2020-02-19 18:29:21 +0000

    Added algorithm for team 13.

--- team13.py
+++ team13.py
@@ -6,25 +6,28 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Hee Hee' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'hee hee' # Only 10 chars displayed.
+strategy_name = 'Betray very often, collude occasionally'
+strategy_description = 'There is a small chance the program colludes on its own, otherwise it will betray. Also betrays if the other person betrays.'
+    
+import random
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
     '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if 'c' in their_history[-10:]: # If the other player has betrayed within last 10 rounds, 
+        return 'b'               # Betray.
+    else:
+        if random.random()<0.4: # 10% of the other rounds
+            return 'c'         # collude
+        else:
+            return 'b'         # but 90% of the time betray
 
